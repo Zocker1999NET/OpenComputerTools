b625219aead208fd2093edd8af51b12a47e0f216
Felix Stupp <zocker.net@live.de>
2017-05-13 20:22:18 +0200

    Implemented Boot Menu

--- rom.lua
+++ rom.lua
@@ -1,136 +1,186 @@
---V2
-local component = component
+--V3
+local component=component
 if not component then
-	component = require("component")
+	component=require("component")
 end
-local computer = computer
+local computer=computer
 if not computer then
-	computer = require("computer")
+	computer=require("computer")
 end
-local b = computer.beep
-local cT = component.type
-local cL = component.list
-local cP = component.proxy
-local cI = component.invoke
-
+local tmp=computer.tmpAddress()
+local b=computer.beep
+local uT=computer.uptime
+local cT=component.type
+local cL=component.list
+local cP=component.proxy
+local cI=component.invoke
 local function cID(dT)
 	return cL(dT)()
 end
 
 function bI(address,method,...)
-	local result = table.pack(pcall(cI,address,method,...))
+	local result=table.pack(pcall(cI,address,method,...))
 	if not result[1] then
 		return nil, result[2]
 	else
 		return table.unpack(result,2,result.n)
 	end
 end
-boot_invoke = bI
+boot_invoke=bI
 
-local eeprom = cID("eeprom")
+local eeprom=cID("eeprom")
 bI(eeprom,"setLabel","Lua Bios with NetBoot")
-computer.getBootAddress = function()
-	local d = bI(eeprom,"getData")
-	if d:len() == 36 then
+local function gBA()
+	local d=bI(eeprom,"getData")
+	if d:len()==36 then
 		return d:sub(1,36)
 	elseif d:len() > 36 then
-		local p = 38
+		local p=38
 		while d:sub(p,p) ~= "|" do
-			p = p + 1
+			p=p + 1
 		end
 		return d:sub(1,36),d:sub(38,p-1),d:sub(p+1)
 	else
 		return nil,"Data corrupted"
 	end
 end
-computer.setBootAddress = function(d,pub,pr)
+local function sBA(d,pub,pr)
 	if not d then
-		d = ""
+		d=""
 	end
 	if not cT(d) then
 		return false
 	end
-	if cT(d) == "modem" and pub and pr then
-		d = d.."|"..tostring(pub).."|"..tostring(pr)
+	if cT(d)=="modem" and pub and pr then
+		d=d.."|"..tostring(pub).."|"..tostring(pr)
 	end
 	return (d ~= nil and bI(eeprom,"setData",d)) or false
 end
+computer.getBootAddress=gBA
+computer.setBootAddress=sBA
 
 local g
-local screen = cID("screen")
-local gpu = cID("gpu")
+local screen=cID("screen")
+local gpu=cID("gpu")
+local kb=cID("keyboard")
 if gpu and screen then
-	g = cP(gpu)
+	g=cP(gpu)
 	g.bind(screen)
 	bI(screen,"turnOn")
 	g.setResolution(50,16)
 	g.setBackground(0)
 	g.setForeground(0xFFFFFF)
 else for i=1,5,1 do b(1500,.1) end end
-local y = 1
-local function print(t)
+local y=0
+local function pr(t)
 	if g then
-		t = tostring(t)
-		if y == 16 then
-			g.copy(1,2,50,15,0,-1)
-			g.fill(1,16,50,1," ")
-		else
-			y = y + 1
+		t=tostring(t)
+		if y==17 then
+			--g.copy(1,2,50,15,0,-1)
+			--g.fill(1,16,50,1," ")
+			y=y-1
 		end
 		g.set(1,y,t)
+		y=y+1
 	end
 end
-------123456789.123456789.123456789.123456789.123456789
-print"Lua BIOS with NetBoot"
-print"by zocker1999net"
-print""
-print"Press [CTRL] for boot menu"
-b(1000,.5)
+local function cls(s) s=s or 1 if g then g.fill(1,s,50,16," ") y=s end end
+local function key(t,...)
+	local k={}
+	for v in pairs({...}) do k[v]=1 end
+	t=uT() + t
+	while t > uT() do
+		local e={computer.pullSignal(t-uT())}
+		if e[1]=="key_down" and k[e[3]] then return e[3] end
+	end
+end
 
-local function tryLoadFrom(add)
-	local t = cT(add)
-	if t == "filesystem" then
-		local h,r = bI(add, "open", "/init.lua")
+local function tLF(a,b,c)
+	local t=cT(a)
+	if t=="filesystem" then
+		local h,r=bI(a,"open","/init.lua")
 		if not h then
-			return nil, r
+			return nil,r
 		end
-		local b = ""
+		local l=""
 		repeat
-			local d,r = bI(add, "read", h, math.huge)
+			local d,r=bI(a,"read",h,math.huge)
 			if not d and r then
-				return nil, r
+				return nil,r
 			end
-			b = b..(d or "")
+			l=l..(d or "")
 		until not d
-		bI(add,"close",h)
-		return load(b,"=init")
-	elseif t == "modem" then
+		bI(a,"close",h)
+		return load(l,"=init")
+	elseif t=="modem" then
 
 	end
 end
-local i, r
-if computer.getBootAddress() then
-	i, r = tryLoadFrom(computer.getBootAddress())
+local function sB()
+	if g then
+		pr"Which source do you like to boot from? (10sec)"
+		pr""
+		pr"(1) Boot from local filesystem"
+		pr"(2) Boot from network"
+		local k=key(10,2,3)
+		if not k then return end
+		li(k==3)
+	end
+end
+local function rB(n)
+	cls(3)
+	local i=0
+	local k={}
+	local l={}
+	for a in cL(n and "modem" or "filesystem") do
+		if bI(a,"exists","/init.lua") then i=i+1 k[i]=i+1 l[i+1]=a print("("..i..") "..a..bI(a,"getLabel")) end
+	end
+	k=key(10,table.unpack(k))
+	if not k then return end
+	local b
+	local c
+	if n then
+
+	end
+	tLF(l[k],b,c)
+end
+pr"Lua Bios with NetBoot"
+pr"by zocker1999net"
+if kb then
+	pr""
+	pr"Press [CTRL] for boot menu"
+	b(1000,.5)
+	if key(.5,29) then
+		cls()
+		sB()
+		computer.turnOff()
+		return nil
+	end
+end
+
+local i,r
+if gBA() then
+	i,r=tLF(gBA())
 end
 if not i then
-	computer.setBootAddress()
+	sBA()
 	for add in component.list("filesystem") do
-		i, r = tryLoadFrom(add)
+		i,r=tLF(add)
 		if i then
-			computer.setBootAddress(add)
+			sBA(add)
 			break
 		end
 	end
 	--[[for add in component.list("modem") do
-		i, r = tryLoadFrom(add)
+		i,r=tLF(add)
 		if i then
-			computer.setBootAddress(add)
+			sBA(add)
 			break
 		end
 	end]]
 end
 if not i then
-	error("no bootable medium found" .. (r and (": " .. tostring(r)) or ""), 0)
+	error("no bootable medium found"..(r and(": "..tostring(r))or""),0)
 end
 b(1000,.2)
 i()
