2a36970ff5ec07bfc62a87a403279cb1a491124d
Felix Stupp <zocker.net@live.de>
2017-05-16 23:08:50 +0200

    Changed config order

--- secDoors.lua
+++ secDoors.lua
@@ -11,22 +11,19 @@ local function lookUpAddresses(tab)
 		tI[n] = k
 	end
 	for i = 1,n,1 do
-		local a = com.get(tab[tI[i]])
-		if not a then
-			error("Device starting with "..tab[tI[i]].." not found!")
+		local v = tab[tI[i]]
+		if type(v) == "string" then
+			local a = com.get(v)
+			if not a then
+				error("Device starting with "..v.." not found!")
+			end
+			tab[tI[i]] = a
 		end
-		tab[tI[i]] = a
 	end
 end
 
-local function changeOutput(dev,out)
-	dev = com.get(dev)
-	if not dev then
-		return false
-	end
-	for s = 1,5,1 do
-		com.invoke(dev,"setOutput",s,(out and 15) or 0)
-	end
+local function changeOutput(tab,out)
+	com.invoke(tab.rsBlock,"setOutput",tab.rsSide,(out and 15) or 0)
 	return true
 end
 
@@ -44,6 +41,7 @@ local function readFile(path)
 	return buffer
 end
 
+print("Load config ...")
 local config = ser.unserialize(readFile("/home/secDoors.cfg"))
 lookUpAddresses(config.mainCom)
 for k,v in pairs(config.gpus) do
@@ -52,10 +50,18 @@ end
 for k,v in pairs(config.doors) do
 	lookUpAddresses(v)
 end
-
+local indexChange = {"screen1","screen2","isForAll1","isForAll2","rsBlock","rsSide"}
+for _,t in pairs(config.doors) do
+	for k,v in pairs(indexChange) do
+		t[v] = t[k]
+	end
+end
 for t,a in pairs(config.mainCom) do
 	com.setPrimary(t,com.get(a))
 end
+
+print("Open all doors ...")
 for n,t in pairs(config.doors) do
-	changeOutput(t[2],true)
+	print(" "..n)
+	changeOutput(t,true)
 end
